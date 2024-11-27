5e9467632d82e0302596c1959b48b91bbc8d873f
portosatos <emirdastanov8@gmail.com>
2024-11-27 21:42:32 +0600

    first working project

--- lib/main.dart
+++ lib/main.dart
@@ -5,94 +5,478 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(""),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.shopping_cart_outlined),
-              onPressed: () {},
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    if (index == 2) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => BonusCardPage()),
+      );
+    } else if (index == 4) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => ProfilePage()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(""),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart_outlined),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: List.generate(5, (index) => const Icon(Icons.image, size: 50)),
             ),
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              // Top placeholder for icons or banners
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: List.generate(5, (index) => Icon(Icons.image, size: 50)),
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
               ),
-              SizedBox(height: 20),
-              // QR code and points section
-              Container(
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  children: [
-                    // QR Code Placeholder
-                    Container(
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => BonusCardPage()),
+                      );
+                    },
+                    child: Container(
                       width: 60,
                       height: 60,
                       color: Colors.black12,
-                      child: Icon(Icons.qr_code, size: 40),
+                      child: const Icon(Icons.qr_code, size: 40),
                     ),
-                    SizedBox(width: 16),
-                    // Points Display
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Бонусы", style: TextStyle(fontSize: 16)),
-                        Text("0.00 баллов", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                      ],
+                  ),
+                  const SizedBox(width: 16),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Бонусы", style: TextStyle(fontSize: 16)),
+                      Text("0 баллов", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text("Популярные товары", style: TextStyle(fontSize: 18)),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return Container(
+                    color: Colors.grey[200],
+                    child: const Center(
+                      child: Icon(Icons.shopping_bag, size: 40),
                     ),
-                  ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Главная"),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: "Уведомления"),
+          BottomNavigationBarItem(icon: Icon(Icons.qr_code), label: "QR-код"),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: "История"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Профиль"),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings Page'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.grey[400],
+                child: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                  size: 40,
                 ),
               ),
-              SizedBox(height: 20),
-              // Popular products section
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text("Популярные товары", style: TextStyle(fontSize: 18)),
-              ),
-              SizedBox(height: 10),
-              Expanded(
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 10,
-                    crossAxisSpacing: 10,
+              SizedBox(height: 8.0),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SettingsPage()),
+                  );
+                },
+                child: Text(
+                  'Ayana',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
                   ),
-                  itemCount: 4,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      color: Colors.grey[200],
-                      child: Center(
-                        child: Icon(Icons.shopping_bag, size: 40),
-                      ),
-                    );
-                  },
+                ),
+              ),
+              SizedBox(height: 4.0),
+              GestureDetector(
+                onTap: () {
+                  // Логика для редактирования
+                },
+                child: Text(
+                  'edit',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              _buildTextField('Number'),
+              SizedBox(height: 8.0),
+              _buildTextField('Email'),
+              SizedBox(height: 8.0),
+              _buildTextField('Регион'),
+              SizedBox(height: 8.0),
+              _buildTextField('Адрес'),
+              SizedBox(height: 8.0),
+              _buildTextField('Уведомления'),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Логика удаления аккаунта
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey[800],
+                  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                child: Text(
+                  'Delete account',
+                  style: TextStyle(color: Colors.white),
                 ),
               ),
             ],
           ),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Главная"),
-            BottomNavigationBarItem(icon: Icon(Icons.notifications), label: "Уведомления"),
-            BottomNavigationBarItem(icon: Icon(Icons.qr_code), label: "QR-код"),
-            BottomNavigationBarItem(icon: Icon(Icons.history), label: "История"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Профиль"),
+      ),
+    );
+  }
+
+  Widget _buildTextField(String hint) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: hint,
+        filled: true,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide.none,
+        ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      ),
+    );
+  }
+}
+
+class BonusCardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 300,
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.grey[400],
+                child: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Container(
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.qr_code,
+                      size: 60,
+                      color: Colors.black,
+                    ),
+                    SizedBox(width: 16.0),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Бонусы',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '0 баллов',
+                          style: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8.0),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool _allowNotifications = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings Page'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: Container(
+        color: Color.fromARGB(255, 238, 232, 232),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Name',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            Divider(),
+            Text(
+              'Umar',
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Phone number',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            Divider(),
+            Text(
+              '+96 XXX XXX XXX',
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Email',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            Divider(),
+            Text(
+              'umar_dont_do_this@gmail.com',
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Birthday date',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            Divider(),
+            Text(
+              'Not specified',
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Allow notifications',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                  ),
+                ),
+                Switch(
+                  value: _allowNotifications,
+                  onChanged: (value) {
+                    setState(() {
+                      _allowNotifications = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 16.0),
+            GestureDetector(
+              onTap: () {
+                // Nav to lang choose page
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Application language',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  Icon(Icons.arrow_forward_ios, size: 16.0),
+                ],
+              ),
+            ),
+            SizedBox(height: 16.0),
+            GestureDetector(
+              onTap: () {
+                // LogOut logic
+              },
+              child: Text(
+                'Log out of profile',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+            Spacer(),
+            GestureDetector(
+              onTap: () {
+                // Delete logic
+              },
+              child: Text(
+                'Delete profile',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
           ],
         ),
       ),
